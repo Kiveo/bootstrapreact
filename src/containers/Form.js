126d044e89547c5fea87d96e5366c2bddef3d714
Mav <mavericktesini@gmail.com>
2018-12-08 10:32:36 -0500

    translate body script to jsx friendly form submit

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -10,24 +10,13 @@ class Form extends Component {
     }
   }
 
-  componentDidMount() {
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (function() {
-      window.addEventListener('load', function() {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-          form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-              event.preventDefault();
-              event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-          }, false);
-        });
-      }, false);
-    })();
+  handleSubmit = (event) => {
+    console.log("ping!");
+    if (event.target.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    event.target.classList.add('was-validated');
   }
   
   handleChange = (event) => {
@@ -40,7 +29,7 @@ class Form extends Component {
   render() {
     console.log(this.state);
     return(
-      <form className="needs-validation" noValidate>
+      <form className="needs-validation" noValidate onSubmit={this.handleSubmit}>
         <div className="form-row">
           <div className="col-md-4 mb-3">
             <label htmlFor="validationTooltip01">First name</label>
