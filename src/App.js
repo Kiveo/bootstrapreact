e88ad13705ac3c1583394b6ac25c29f380c02652
Mav <mavericktesini@gmail.com>
2018-12-08 11:03:53 -0500

    create sample2 page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './containers/Navbar';
 import { About } from './components/About';
 import { Footer } from './components/Footer';
 import { Sample1 } from './components/Sample1';
+import { Sample2 } from './components/Sample2';
 
 import './App.css';
 
@@ -19,6 +20,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route exact path='/about' component={About} />
             <Route path ="/sample1" component={Sample1} />
+            <Route path ="/sample2" component={Sample2} />
           </Switch>
 
           <Footer />
---
+++ src/components/Sample2.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Jumbotron from '../containers/Jumbotron';
+import Form from '../containers/Form';
+
+export const Sample2 = () => {
+  return(
+    <div className="page">
+      {window.scrollTo(0, 0)}
+      <Jumbotron title="Sample 2 Prop" subtitle="Sample 2 Subtitle" />
+      <div className="container">
+        <hr />
+
+        <h2>Sample 2</h2>
+
+      </div>
+    </div>
+  );
+}
