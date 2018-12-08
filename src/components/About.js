5082cf2072e53d95c5c32ca550fb1e996037d053
Mav <mavericktesini@gmail.com>
2018-12-08 10:57:25 -0500

    declutter content on about page

--- src/components/About.js
+++ src/components/About.js
@@ -8,14 +8,8 @@ export const About = () => {
       {window.scrollTo(0, 0)}
       <Jumbotron title="About Prop" subtitle="About Subtitle" />
       <div className="container">
-        <div className="row">
-          <Card size="col-md-4" heading="Card 1" />
-          <Card size="col-md-4" heading="Card 2" />
-          <Card size="col-md-4" heading="Card 3" />
-        </div>
         <hr />
-
-        <h2>Additional Info</h2>
+        <h2>Pertinent Info</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
           Phasellus volutpat lectus eget dolor posuere facilisis. 
           Sed aliquam lacinia mauris non sollicitudin. Sed ultrices sed lorem eu sollicitudin. 
