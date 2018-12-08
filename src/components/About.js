cec1b408e8798b0b0b147b0477e74eeedb6ad572
Mav <mavericktesini@gmail.com>
2018-12-08 07:30:51 -0500

    mimic home content on about

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Card } from './Card';
+import Jumbotron from '../containers/Jumbotron';
 
 export const About = () => {
   return(
-    <div className="container">
-      <h2>ABOUT PAGE</h2>
-      <p>Useful information about the web application goes here.</p>
+    <div className="page">
+      <Jumbotron title="About Prop" subtitle="About Subtitle" />
+      <div className="container">
+        <div className="container">
+          <div className="row">
+            <Card size="col-md-4" heading="Card 1" />
+            <Card size="col-md-4" heading="Card 2" />
+            <Card size="col-md-4" heading="Card 3" />
+          </div>
+          <hr />
+        </div>
+
+        <h2>Additional Info</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Phasellus volutpat lectus eget dolor posuere facilisis. 
+          Sed aliquam lacinia mauris non sollicitudin. Sed ultrices sed lorem eu sollicitudin. 
+          Curabitur egestas massa quis venenatis tincidunt. Nullam mollis nisi nec hendrerit posuere. 
+          Aenean lorem velit, feugiat in feugiat sed, feugiat nec mi. Nulla at finibus justo.</p>
+          
+      </div>
     </div>
   );
 }
