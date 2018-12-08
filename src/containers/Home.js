b1d6742e2c361442646d47f12eff84eef4cb8a15
Mav <mavericktesini@gmail.com>
2018-12-08 06:17:21 -0500

    add image as jumbo bg, adjust jumbo into layout

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,52 +6,54 @@ import Jumbotron from './Jumbotron';
 class Home extends Component {
   render() {
     return(
-      <div className="container">
-        <Jumbotron />
-
+      <React.Fragment>
+        <Jumbotron title="Prop Heading" subtitle="Subtitle Prop" />
         <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <h2>Heading 1</h2>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-              <p><Link className="btn btn-secondary" to="/heading1">View details »</Link></p>
-            </div>
-            <div className="col-md-4">
-              <h2>Heading 2</h2>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-              <p><Link className="btn btn-secondary" to="/heading2">View details »</Link></p>
-            </div>
-            <div className="col-md-4">
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-              <p><Link className="btn btn-secondary" to="/heading3">View details »</Link></p>
-            </div>
-          </div>
 
-          <hr />
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <h2>Heading 1</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><Link className="btn btn-secondary" to="/heading1">View details »</Link></p>
+              </div>
+              <div className="col-md-4">
+                <h2>Heading 2</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><Link className="btn btn-secondary" to="/heading2">View details »</Link></p>
+              </div>
+              <div className="col-md-4">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <p><Link className="btn btn-secondary" to="/heading3">View details »</Link></p>
+              </div>
+            </div>
+
+            <hr />
+          </div>
+          
+          <h2>Welcome</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Phasellus volutpat lectus eget dolor posuere facilisis. 
+            Sed aliquam lacinia mauris non sollicitudin. Sed ultrices sed lorem eu sollicitudin. 
+            Curabitur egestas massa quis venenatis tincidunt. Nullam mollis nisi nec hendrerit posuere. 
+            Aenean lorem velit, feugiat in feugiat sed, feugiat nec mi. Nulla at finibus justo.</p>
+          
+          <p>Aenean vitae ante non diam auctor malesuada sed sit amet nunc. 
+            Donec eleifend nibh eu finibus vulputate. Aliquam tempus tempor dui. 
+            Integer hendrerit, ante at sagittis posuere, justo velit posuere odio, 
+            sed dignissim turpis lectus vel diam. Fusce et facilisis arcu. 
+            Aliquam sollicitudin neque vitae augue euismod pretium. 
+            Nulla tincidunt ultricies elit, at pulvinar enim cursus eget. 
+            Curabitur nunc libero, ultrices eget suscipit non, vulputate at odio. 
+            Cras convallis nisl nisi, at laoreet nisl accumsan nec. 
+            Pellentesque commodo lorem in ante iaculis dictum. 
+            Duis metus tortor, varius posuere efficitur non, tincidunt in sapien. 
+            Curabitur sed nisi erat. Nam nec pretium nibh, sed ultrices dui. 
+            Etiam facilisis tempor justo vel aliquet. 
+            Quisque ligula metus, mollis vitae euismod eu, molestie elementum metus.</p>
         </div>
-        
-        <h2>Welcome</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Phasellus volutpat lectus eget dolor posuere facilisis. 
-          Sed aliquam lacinia mauris non sollicitudin. Sed ultrices sed lorem eu sollicitudin. 
-          Curabitur egestas massa quis venenatis tincidunt. Nullam mollis nisi nec hendrerit posuere. 
-          Aenean lorem velit, feugiat in feugiat sed, feugiat nec mi. Nulla at finibus justo.</p>
-        
-        <p>Aenean vitae ante non diam auctor malesuada sed sit amet nunc. 
-          Donec eleifend nibh eu finibus vulputate. Aliquam tempus tempor dui. 
-          Integer hendrerit, ante at sagittis posuere, justo velit posuere odio, 
-          sed dignissim turpis lectus vel diam. Fusce et facilisis arcu. 
-          Aliquam sollicitudin neque vitae augue euismod pretium. 
-          Nulla tincidunt ultricies elit, at pulvinar enim cursus eget. 
-          Curabitur nunc libero, ultrices eget suscipit non, vulputate at odio. 
-          Cras convallis nisl nisi, at laoreet nisl accumsan nec. 
-          Pellentesque commodo lorem in ante iaculis dictum. 
-          Duis metus tortor, varius posuere efficitur non, tincidunt in sapien. 
-          Curabitur sed nisi erat. Nam nec pretium nibh, sed ultrices dui. 
-          Etiam facilisis tempor justo vel aliquet. 
-          Quisque ligula metus, mollis vitae euismod eu, molestie elementum metus.</p>
-      </div>
+      </React.Fragment>
     )
   }
 }
--- src/containers/Jumbotron.js
+++ src/containers/Jumbotron.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './Jumbotron.css';
 
 class Jumbotron extends Component {
   render() {
     return(
-      <div className="jumbotron">
+      <div className="jumbotron jumbotron-fluid">
         <div className="container">
-          <h1 className="display-3">Hello, world!</h1>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-          <p><Link className="btn btn-primary btn-lg" to="#" role="button">Learn more »</Link></p>
+          <h1 className="display-2">{this.props.title}</h1>
+          <p className="lead">{this.props.subtitle}</p>
         </div>
       </div>
     );
